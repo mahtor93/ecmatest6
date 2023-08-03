34954ab9342c542d0acd1ab82544d5b9d94fd82a
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-08-03 09:48:33 -0400

    04-rest-spread rest ejemplo

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -1,5 +1,4 @@
 //arrays destrucuring
-
 let frutas = ['Manzana', 'Platano'];
 let [a,b] = frutas; //desestructurando el array para su uso más práctico.
 console.log(a,b);
@@ -7,9 +6,17 @@ console.log(a);
 console.log(b);
 
 //desestructuración de objetos
-
 let user =  { username: ' Test', age:33};
 let {username,age}= user;
 console.log(username);
 console.log(age);
 console.log(user.age);
+
+//spread operator
+let person = { name: 'Test2', age: 66}
+let country = 'MX';
+let data = { ...person, country } //agregando valores de un objeto a otro.
+console.log(person);
+console.log(data);
+
+//rest
